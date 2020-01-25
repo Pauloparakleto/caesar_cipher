416df6cccc8c1b15d36711f17fd66528f8f74152
Pauloparakleto <alparakleto@gmail.com>
2020-01-25 16:38:28 -0300

    setting :root as app.rb

--- app.rb
+++ app.rb
@@ -1,14 +1,19 @@
 require 'sinatra'
-if development? require 'sinatra/reloader'  #Heroku not to use it.
+if development?
+     require 'sinatra/reloader'  #Heroku not to use it.
+end
 require './lib/caesar_cipher'
 
+set :root, File.dirname(__FILE__)
+
 get '/' do
-    word = params['word']
+    word = params['word'] || '.'
     number = params['number'].to_i
     code = caesar_cipher(word, number)
 
     erb :index, :locals => {:word => word, :number => number, :code => code}
 
 end
+
 
 #puts cript.caesar_cipher("What a string!", 5) == "Bmfy f xywnsl!"
